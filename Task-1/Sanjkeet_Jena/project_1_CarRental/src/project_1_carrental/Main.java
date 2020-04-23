2948e4b4d2b2f1061c0f43a9c7332335ea4d61af
ZerothKing <iamkeet.191cs246@nitk.edu.in>
2020-04-23 11:57:27 +0530

    Add files for task 1

---
+++ Task-1/Sanjkeet_Jena/project_1_CarRental/src/project_1_carrental/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package project_1_carrental;
+
+/**
+ *
+ * @author Sanjkeet
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
